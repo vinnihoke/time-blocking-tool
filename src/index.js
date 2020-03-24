0155110ae239b9bf072bf2b2830058251edf9c07
Vinni Hoke <vinni.hoke@gmail.com>
2020-03-24 13:07:18 -0700

    Setting up ui-cleanup branch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
 import 'semantic-ui-css/semantic.min.css'
 import "react-datepicker/dist/react-datepicker.css";
 import './index.scss';
